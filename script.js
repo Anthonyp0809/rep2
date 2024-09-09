8b0352bb75bcfa5d0be18936bb0fd433105c8222
Anthony0809 <evapacheco20@yahoo.com>
2024-09-09 13:27:43 -0700

    quizz website

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+$(document).ready(function(){
+	
+	
+	$(".correct").click(function(){
+		$(this).css("color","green");
+	});
 
+	$(".incorrect").click(function(){
+			$(this).css("color","red");
+	});
+
+});
